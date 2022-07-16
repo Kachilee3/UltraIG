180f78e7fd9b9444a3e499f8b82efc72ad003dae
Kachi Lee <67455277+Kachilee3@users.noreply.github.com>
2022-07-16 17:57:43 +0100

    Update Ultra.py

--- Ultra.py
+++ Ultra.py
@@ -1,1 +1,29 @@
-N
+import os
+import platform
+import webbrowser
+os.system('termux-setup-storage')
+
+
+os.system('git pull')
+try:os.system('mkdir Music')
+except:pass
+try:os.system('touch .ua.txt')
+except:pass
+P = '\x1b[1;97m'
+import os,requests
+xr = requests.get("http://ip-api.com/json/").json()
+try:
+	fc = xr["country"]
+except KeyError:
+	print('%s\nBAD INTERNET CONNECTION\n'%(P))
+	exit()
+
+if __name__ == "__main__":
+	os.system("git pull")
+	if "Nigeria" == fc:
+		__import__("Ultra").license()
+	else:
+		__import__("Ultra").license()
+except requests.exceptions.ConnectionError:
+	exit(f'\n [{M}!{C}] Koneksi internet bermasalah')
+
