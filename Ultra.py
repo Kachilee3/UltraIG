c8fef3be98e202430b0882aec4b9473145a952ce
Kachi Lee <67455277+Kachilee3@users.noreply.github.com>
2022-07-16 17:46:25 +0100

    Update Ultra.py

--- Ultra.py
+++ Ultra.py
@@ -21,6 +21,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("Ultra").license()
+		__import__("Ultra").license , licenses()
 	else:
-		__import__("Ultra").license()
+		__import__("Ultra").license , licenses()
